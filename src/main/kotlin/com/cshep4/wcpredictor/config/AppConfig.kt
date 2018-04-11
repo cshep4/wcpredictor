aad54156b4f7322f4b4679fe9197ef5bb2cdcc36
cshep4 <chris_shepherd2@hotmail.com>
2018-04-11 11:47:39 +0100

    Added cors config

--- src/main/kotlin/com/cshep4/wcpredictor/config/AppConfig.kt
+++ src/main/kotlin/com/cshep4/wcpredictor/config/AppConfig.kt
@@ -3,11 +3,41 @@ package com.cshep4.wcpredictor.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+import javax.servlet.Filter
+
 
 @Configuration
 class AppConfig {
     @Bean
     fun bCryptPasswordEncoder(): BCryptPasswordEncoder {
         return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    fun corsFilter(): Filter {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.addAllowedOrigin("*")
+        config.addAllowedHeader("Access-Control-Allow-Headers")
+        config.addAllowedHeader("Content-Type")
+        config.addAllowedHeader("x-xsrf-token")
+        config.addAllowedHeader("Authorization")
+        config.addAllowedHeader("Origin")
+        config.addAllowedHeader("Accept")
+        config.addAllowedHeader("X-Requested-With")
+        config.addAllowedHeader("Access-Control-Request-Method")
+        config.addAllowedHeader("Access-Control-Request-Headers")
+        config.addAllowedMethod("OPTIONS")
+        config.addAllowedMethod("GET")
+        config.addAllowedMethod("PUT")
+        config.addAllowedMethod("POST")
+        config.addAllowedMethod("DELETE")
+        source.registerCorsConfiguration("/**", config)
+
+        return CorsFilter(source)
     }
 }
