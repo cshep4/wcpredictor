f277f31f5b924535d63e5c283450892e6b51d5aa
cshep4 <chris_shepherd2@hotmail.com>
2018-06-15 19:44:19 +0100

    Fixed problem with matches not been predicted

--- src/main/kotlin/com/cshep4/wcpredictor/service/fixtures/FixturesApiService.kt
+++ src/main/kotlin/com/cshep4/wcpredictor/service/fixtures/FixturesApiService.kt
@@ -2,13 +2,13 @@ package com.cshep4.wcpredictor.service.fixtures
 
 import com.cshep4.wcpredictor.data.api.FixturesApiResult
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.httpGet
 import org.springframework.stereotype.Service
 
 @Service
 class FixturesApiService {
     fun retrieveFixtures(url: String, headerKey: String, headerValue: String): FixturesApiResult? {
-        val (req, res, result) = Fuel.get(url, listOf(headerKey to headerValue)).responseString()
+        val (req, res, result) = url.httpGet(listOf(headerKey to headerValue)).responseString()
 
         return result.fold({ data ->
             return@fold ObjectMapper().readValue(data, FixturesApiResult::class.java)
