d6f5b299fee26c06aee332d05419ee64fad0b133
Chris Shepherd <chris_shepherd2@hotmail.com>
2018-06-15 19:43:33 +0100

    Fixed problem with matches not been predicted

--- src/main/kotlin/com/cshep4/wcpredictor/extension/MatchExtensions.kt
+++ src/main/kotlin/com/cshep4/wcpredictor/extension/MatchExtensions.kt
@@ -3,5 +3,5 @@ package com.cshep4.wcpredictor.extension
 import com.cshep4.wcpredictor.data.PredictedMatch
 
 fun List<PredictedMatch>.getMatchById(id: Long?): PredictedMatch {
-    return this.firstOrNull{ it.id == id } ?: PredictedMatch(id = id!!, aGoals = null, hGoals = null)
+    return this.first { it.id == id }
 }
