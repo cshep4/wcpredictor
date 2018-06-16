52a2fb7c5d06e02cadfedb5a47ef05f79b70fbde
cshep4 <chris_shepherd2@hotmail.com>
2018-06-16 13:33:02 +0100

    REVERTTED FIX

--- src/main/kotlin/com/cshep4/wcpredictor/component/fixtures/PredictionMerger.kt
+++ src/main/kotlin/com/cshep4/wcpredictor/component/fixtures/PredictionMerger.kt
@@ -3,20 +3,21 @@ package com.cshep4.wcpredictor.component.fixtures
 import com.cshep4.wcpredictor.data.Match
 import com.cshep4.wcpredictor.data.PredictedMatch
 import com.cshep4.wcpredictor.data.Prediction
+import com.cshep4.wcpredictor.extension.getMatchById
 import org.springframework.stereotype.Component
 
 @Component
 class PredictionMerger {
     fun merge(matches: List<Match>, predictions: List<Prediction>): List<PredictedMatch> {
         val predictedMatches = matches.map { it.toPredictedMatch() }
-//        predictions.forEach { predictedMatches.getMatchById(it.matchId).updatePrediction(it.id, it.hGoals, it.aGoals) }
+        predictions.forEach { predictedMatches.getMatchById(it.matchId).updatePrediction(it.id, it.hGoals, it.aGoals) }
 
-        predictedMatches.forEach {
-            val id = it.id
-            val prediction = predictions.firstOrNull{ it.matchId == id } ?: Prediction(matchId = id, hGoals = null, aGoals = null)
-
-            it.updatePrediction(null, prediction.hGoals, prediction.aGoals)
-        }
+//        predictedMatches.forEach {
+//            val id = it.id
+//            val prediction = predictions.firstOrNull{ it.matchId == id } ?: Prediction(matchId = id, hGoals = null, aGoals = null)
+//
+//            it.updatePrediction(null, prediction.hGoals, prediction.aGoals)
+//        }
 
         return predictedMatches
     }
